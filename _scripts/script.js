537096a9690b78a8e4c6fbb598b49f069c63270f
thekinglaolu <afolayan.david7@gmail.com>
2018-04-11 00:40:58 +0100

    add variables for buttons and relevant fetures to be used throughout the script

---
+++ _scripts/script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	let createContact = document.getElementById('createContact');
+	let editContact = document.getElementById('editContact');
+	let deleteContact = document.getElementById('deleteContact');
+	let addContact = document.getElementById('addContact');
+}
